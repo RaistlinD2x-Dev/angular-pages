4bb4a37ad77c27cc7cc725ae242078823a2e38cb
Jesse <RaistlinD2x.dev@outlook.com>
2021-05-19 12:59:26 -0500

    setup class directive

---
+++ src/app/class.directive.ts
@@ -0,0 +1,14 @@
+import { stringify } from '@angular/compiler/src/util';
+import { Directive, ElementRef, Input } from '@angular/core';
+
+@Directive({
+  selector: '[appClass]'
+})
+export class ClassDirective {
+  @Input() backgroundColor: string = '';
+
+  constructor(private element: ElementRef) {
+    this.element.nativeElement.style.backgroundColor = this.backgroundColor
+   }
+
+}
