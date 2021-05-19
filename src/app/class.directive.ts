b2a608f0a0e8fd067f4b777b6d122fd55115ead7
Jesse <RaistlinD2x.dev@outlook.com>
2021-05-19 13:04:48 -0500

    setup directive to pass arguments from HTML elements

--- src/app/class.directive.ts
+++ src/app/class.directive.ts
@@ -5,10 +5,12 @@ import { Directive, ElementRef, Input } from '@angular/core';
   selector: '[appClass]'
 })
 export class ClassDirective {
-  @Input() backgroundColor: string = '';
-
   constructor(private element: ElementRef) {
-    this.element.nativeElement.style.backgroundColor = this.backgroundColor
+    
+   }
+
+   @Input() set backgroundColor(color: string){
+    this.element.nativeElement.style.backgroundColor = color
    }
 
 }
