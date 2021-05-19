0ff2dc118ad9af4ad22b05df61d620a461cf7858
Jesse <RaistlinD2x.dev@outlook.com>
2021-05-19 13:14:40 -0500

    custom directive selector and property set to same name

--- src/app/class.directive.ts
+++ src/app/class.directive.ts
@@ -9,7 +9,7 @@ export class ClassDirective {
     
    }
 
-   @Input() set backgroundColor(color: string){
+   @Input() set appClass(color: string){
     this.element.nativeElement.style.backgroundColor = color
    }
 
