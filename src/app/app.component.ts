69252a2a13cb04d63809e810b53596f44de0a4b2
RaistlinD2x-Dev <RaistlinD2x.dev@outlook.com>
2021-05-18 23:08:55 -0500

    created pagination number list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+  currentPage = 0;
+  images = [
+    {
+      title: 'hot girl 1',
+      url: 'https://images.unsplash.com/photo-1567892320421-1c657571ea4a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YmlraW5pfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60'
+    },
+    {
+      title:'tits and abs',
+      url: 'https://images.unsplash.com/photo-1582639590011-f5a8416d1101?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YmlraW5pfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60'
+    },
+    {
+      title: 'all that ass',
+      url: 'https://images.unsplash.com/photo-1570976447640-ac859083963f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8YmlraW5pfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60'
+    },
+    {
+      title: 'My favorite',
+      url: 'https://images.unsplash.com/photo-1570976447640-ac859083963f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8YmlraW5pfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60'
+    }
+
+  ]
 }
